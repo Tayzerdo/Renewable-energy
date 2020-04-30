5a27152eaadca612dee90f4e2d3243e288d340f8
Tayzerdo <64328213+Tayzerdo@users.noreply.github.com>
2020-04-30 19:59:22 +0100

    Update BP_Stats_Review.py

--- BP_Stats_Review.py
+++ BP_Stats_Review.py
@@ -7,14 +7,10 @@ Created on Wed Apr 29 12:47:51 2020
 import pandas as pb
 
 #Worksheet Primary Energy Consumptioin
-file = pb.read_excel('bp-stats-review-2019-all-data.xlsx', sheet_name = 'Primary Energy Consumption', hearders = 2, skiprows=2, usecols=range(55))
-#print(file)
-
+file = pb.read_excel('bp-stats-review-2019-all-data.xlsx', sheet_name = 'Primary Energy Consumption', hearders = 2, skiprows=2, usecols=range(55)).dropna(axis=0,how='all')
 
 #Find the lines for each categorie (in terms of Region)
+Places = file[~file['Million tonnes oil equivalent'].str.startswith('Total')]
 
-###4,17,53,63,74,85,105 til the end###
-
-Countries = file.drop([4,17,53,63,74,85,105,106,107,108,109,110,111,112,113,114,115,116,117],0).dropna(axis=0,how='all').sort_values(by='Million tonnes oil equivalent')
-
-#print(Countries.describe())
+#Find only the Total values
+Total_Countries = file[file['Million tonnes oil equivalent'].str.startswith('Total')]
